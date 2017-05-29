8fdf1bff8e59a90fe9e5f9567ab05247f04d4a84
Divesh Otwani <divesh.otwani@gmail.com>
2017-05-29 13:01:48 -0400

    now an executable

--- Haskell/TypesAndPL_Pierce/UntypedLambdaCalc/LCalc.hs
+++ Haskell/TypesAndPL_Pierce/UntypedLambdaCalc/LCalc.hs
@@ -1,18 +1,57 @@
 {-# LANGUAGE GADTs, DataKinds #-}
 module LCalc where
 
+import Network.CGI.Protocol
+import System.IO
+
+greeting :: String
+greeting = "Welcome to Divesh's Untyped Lambda Calculus."
+
+interface :: String
+interface =
+  unlines
+    [ "data Term where"
+    , "  Iden :: Var -> Term"
+    , "  Lambda :: Var -> Term -> Term"
+    , "  App :: Term -> Term -> Term"
+    , ""
+    , "data Var where"
+    , "  Var :: String -> Var"
+    ]
+
+
+main :: IO ()
+main = do
+  putStrLn greeting
+  loop
+
+loop :: IO ()
+loop = do
+  putStr "> "
+  hFlush stdout
+  inp <- getLine
+  case (maybeRead inp) :: Maybe Term of
+    Nothing -> do
+      putStrLn "Parse error on input."
+      putStrLn "This is the interface: \n"
+      putStrLn interface
+      loop
+    Just t -> do
+      print $ evaluate t
+      hFlush stdout
+      loop
 
 
 data Term where
   Iden :: Var -> Term
   Lambda :: Var -> Term -> Term
   App :: Term -> Term -> Term
-  deriving (Show, Eq)
+  deriving (Show, Eq, Read)
 
 
 data Var where
   Var :: String -> Var
-  deriving (Show, Eq)
+  deriving (Show, Eq, Read)
 
 -- We use normal evaluation.
 -- That is, left to right.
@@ -46,6 +85,16 @@ evaluate :: Term -> Term
 evaluate t = case oneStepEval t of
   Just t' -> evaluate t'
   Nothing -> t
+
+-- for testing
+true :: Term
+true = Lambda (Var "x") (Lambda (Var "y") (Iden (Var "x")))
+false :: Term
+false = Lambda (Var "x") (Lambda (Var "y") (Iden (Var "y")))
+
+
+
+
 
 
 
---
+++ Haskell/TypesAndPL_Pierce/UntypedLambdaCalc/ToDo.md
@@ -0,0 +1,8 @@
+
+
+The next step is to add variable bindings 
+and allow recursive definitions -- with lazyness.
+
+
+
+
