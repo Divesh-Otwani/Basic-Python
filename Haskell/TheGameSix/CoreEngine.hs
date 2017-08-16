7c1114fd4ebd00a490d9a7e70d2d62ee3d225648
Divesh Otwani <divesh.otwani@gmail.com>
2017-08-15 23:09:07 -0400

    Commit at time Tue Aug 15 23:09:07 EDT 2017

--- Haskell/TheGameSix/CoreEngine.hs
+++ Haskell/TheGameSix/CoreEngine.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, FlexibleInstances #-}
 module CoreEngine where
 
 {-
+
 In this module, I implement the core engine of the game.
 Here, we choose a representation and interface for
 the board and the game in general. Then we ultimately
@@ -17,6 +18,8 @@ commenting. Observe the names carefully.
 
 import MyMatrix ( Matrix, maybeIndexM, maybeSetM
                 , queryDims, prettyPrint)
+import Control.Monad ( guard )
+import Control.Applicative (Alternative, liftA2)
 
 
 data GameState where
@@ -60,7 +63,7 @@ type PieceIndex = (Int, Int)
 -- Bot ~ bottom
 
 type GetNeighborIx = PieceIndex -> PieceIndex
-type RelativeDirection = GetNeighborIx
+type PiecePathFn = GetNeighborIx
 
 ixL :: GetNeighborIx
 ixL (row,col) = (row,col-1)
@@ -80,25 +83,25 @@ ixBotL (row,col) = (row+1,col)
 ixBotR :: GetNeighborIx
 ixBotR (row,col) = (row+1,col+1)
 
-data ConnectedPath where
-  L :: ConnectedPath
-  R :: ConnectedPath
-  TopL :: ConnectedPath
-  TopR :: ConnectedPath
-  BottomL :: ConnectedPath
-  BottomR :: ConnectedPath
+data AbstractPiecePath where
+  L :: AbstractPiecePath
+  R :: AbstractPiecePath
+  TopL :: AbstractPiecePath
+  TopR :: AbstractPiecePath
+  BottomL :: AbstractPiecePath
+  BottomR :: AbstractPiecePath
   ComposedPath ::
-    ConnectedPath -> -- do this path after
-    ConnectedPath -> -- you do this path
-    ConnectedPath
+    AbstractPiecePath -> -- do this path first
+    AbstractPiecePath -> -- and then do this path
+    AbstractPiecePath
   deriving (Show, Eq)
 
-(<.>) :: ConnectedPath -> ConnectedPath -> ConnectedPath
+(<.>) :: AbstractPiecePath -> AbstractPiecePath -> AbstractPiecePath
 (<.>) = ComposedPath
 infixr 5 <.>
 
-pathToDirection :: ConnectedPath -> RelativeDirection
-pathToDirection x = case x of
+absPathToPathFn :: AbstractPiecePath -> PiecePathFn
+absPathToPathFn x = case x of
   L -> ixL
   R -> ixR
   TopL -> ixTopL
@@ -106,9 +109,10 @@ pathToDirection x = case x of
   BottomL -> ixBotL
   BottomR -> ixTopR
   (ComposedPath p q) ->
-    pathToDirection p . pathToDirection q
+    -- Observe the order. We do path p first.
+    absPathToPathFn q . absPathToPathFn p
 
-rotatePath :: ConnectedPath -> ConnectedPath
+rotatePath :: AbstractPiecePath -> AbstractPiecePath
 rotatePath p = case p of
   L -> TopL
   TopL -> TopR
@@ -120,7 +124,7 @@ rotatePath p = case p of
     ComposedPath (rotatePath f) (rotatePath g)
 
 
-pathPieceIx :: PieceIndex -> ConnectedPath -> PieceIndex
+pathPieceIx :: PieceIndex -> AbstractPiecePath -> PieceIndex
 pathPieceIx cell dir =
   case dir of
     L -> ixL cell
@@ -134,34 +138,34 @@ pathPieceIx cell dir =
 
 checkSameColor ::
   PieceIndex -> -- the start cell
-  [RelativeDirection] -> -- paths to other cells
+  [PiecePathFn] -> -- paths to other cells
   SixBoard -> -- the board
   Bool
 -- True iff these cells exist, have pieces, and are the same
 
-checkSameColor cell dirs@(_:_) board =
-  case maybeGetCells cell dirs board of
-    Just pieces@(h:_) -> all (== h) pieces
+checkSameColor cell paths@(_:_) brd =
+  case maybeGetPieces cell paths brd of
+    Just pieces@(fstPiece:_) -> all (== fstPiece) pieces
     Just _ -> False -- something is wrong!
     Nothing -> False
 -- all elems in ∅ are same color
 checkSameColor _ _ _ = True
 
 
-maybeGetCells ::
+maybeGetPieces ::
   PieceIndex ->
-  [RelativeDirection] ->
+  [PiecePathFn] ->
   SixBoard ->
   Maybe [Piece]
-maybeGetCells cell dirs board = do
+maybeGetPieces cell paths brd = do
    let queryPieces =
-         map ((`maybeIndexM` board) . ($ cell)) dirs
+         map ((`maybeIndexM` brd) . ($ cell)) paths
    cellsThatExist <- maybeExtract queryPieces
    cellsThatHavePieces <- maybeExtract cellsThatExist
    return cellsThatHavePieces
 
 
--- Get a list iff all elements present
+-- Get a list iff ALL elements present
 maybeExtract :: [Maybe a] -> Maybe [a]
 maybeExtract [] = Just []
 maybeExtract (x : xs) = do
@@ -179,14 +183,14 @@ maybeExtract (x : xs) = do
 -- Triangle Constructs
 -- This is the crown from the
 -- tip of a triangle
-crown :: [ConnectedPath]
+crown :: [AbstractPiecePath]
 crown = [ TopL
         , TopR
         , TopL <.> TopL
         , TopL <.> TopR
         , TopR <.> TopR
         ]
-rotatePaths :: [ConnectedPath] -> [ConnectedPath]
+rotatePaths :: [AbstractPiecePath] -> [AbstractPiecePath]
 rotatePaths = map rotatePath
 
 waterfall :: (a -> a) -> [a] -> [a]
@@ -194,32 +198,32 @@ waterfall _ [] = []
 waterfall fn (x:xs) =
   fn x : waterfall (fn . fn) xs
 
-rotationsOf :: [ConnectedPath] -> [[ConnectedPath]]
+rotationsOf :: [AbstractPiecePath] -> [[AbstractPiecePath]]
 rotationsOf = waterfall rotatePaths . replicate 6
 
-crowns :: [[ConnectedPath]]
+crowns :: [[AbstractPiecePath]]
 crowns = rotationsOf crown
 
 
 -- This is the arrow surrounding
 -- a center triangle piece
-arrow :: [ConnectedPath]
+arrow :: [AbstractPiecePath]
 arrow = [ TopL
         , TopR
         , TopL <.> TopR
         , L
         , R
         ]
-arrows :: [[ConnectedPath]]
+arrows :: [[AbstractPiecePath]]
 arrows = rotationsOf arrow
 
-triangleChecks :: [[ConnectedPath]]
+triangleChecks :: [[AbstractPiecePath]]
 triangleChecks = crowns ++ arrows
 
 
 -- Circle Constructs
 -- This is the center of a circle
-circCenter :: [ConnectedPath]
+circCenter :: [AbstractPiecePath]
 circCenter = [ TopL
              , TopR
              , L
@@ -228,41 +232,41 @@ circCenter = [ TopL
              , BottomR
              ]
 
-shiftPath :: ConnectedPath -> [ConnectedPath] -> [ConnectedPath]
+shiftPath :: AbstractPiecePath -> [AbstractPiecePath] -> [AbstractPiecePath]
 shiftPath p = map (p <.>)
 
-shiftCircCenter :: ConnectedPath -> [ConnectedPath]
+shiftCircCenter :: AbstractPiecePath -> [AbstractPiecePath]
 shiftCircCenter = (`shiftPath` circCenter)
 
-nbrDirections :: [ConnectedPath]
+nbrDirections :: [AbstractPiecePath]
 nbrDirections = circCenter
 
-circChecks :: [[ConnectedPath]]
+circChecks :: [[AbstractPiecePath]]
 circChecks = map shiftCircCenter nbrDirections
 
 -- Line Constructs
 
-lineEnd :: [ConnectedPath]
+lineEnd :: [AbstractPiecePath]
 lineEnd = R : waterfall (R <.>)  (replicate 4 R)
 
-lineMiddle :: [ConnectedPath]
+lineMiddle :: [AbstractPiecePath]
 lineMiddle = L : take 4 lineEnd
 
-lineCenter :: [ConnectedPath]
+lineCenter :: [AbstractPiecePath]
 lineCenter = (L <.> L) : take 4 lineMiddle
 
-lineTypes :: [[ConnectedPath]]
+lineTypes :: [[AbstractPiecePath]]
 lineTypes = [lineEnd, lineMiddle, lineCenter]
 
-lineChecks :: [[ConnectedPath]]
+lineChecks :: [[AbstractPiecePath]]
 lineChecks = concatMap rotationsOf lineTypes
 
-checksToDo :: [[ConnectedPath]]
+checksToDo :: [[AbstractPiecePath]]
 checksToDo =
   triangleChecks ++ circChecks ++ lineChecks
 
-dirsOfChecksToDo :: [[RelativeDirection]]
-dirsOfChecksToDo = (map . map) pathToDirection checksToDo
+pathFnsOfChecksToDo :: [[PiecePathFn]]
+pathFnsOfChecksToDo = (map . map) absPathToPathFn checksToDo
 
 
 --- Finally, to check if the game is won:
@@ -272,7 +276,7 @@ checkWinAt cell brd =
   let
     colorChecker x = checkSameColor cell x brd
   in
-    any colorChecker dirsOfChecksToDo
+    any colorChecker pathFnsOfChecksToDo
 
 
 --------------------------------------------------------------------
@@ -283,7 +287,21 @@ checkWinAt cell brd =
 
 data Error a where
   Error :: a -> Error a
+
 type SimpleError = Error String
+type MaybeError = Either SimpleError
+
+instance Applicative MaybeError where
+  pure = Right
+
+  liftA2 fn (Right a) (Right b) = pure $ fn a b
+  liftA2 _  (Left e)  (Right _) = Left e
+  liftA2 _   _        someError = someError
+
+
+
+instance Alternative (Either SimpleError) where
+
 
 data GameMove where
   -- In phase one, you just place
@@ -295,33 +313,74 @@ data GameMove where
 
 type Hopefully a = Either SimpleError a
 
-makeMove :: GameMove -> GameState -> Hopefully GameState
+-- Convention: (red, black)
+type SixPlayers = (PlayerData, PlayerData)
+type PrevPlayerData = SixPlayers
+type NextPlayerData = SixPlayers
+
+type PrevBoard = SixBoard
+type NextBoard = SixBoard
+
+type PrevPhase = GamePhase
+type NextPhase = GamePhase
+
+type PrevStatus = GameStatus
+type NextStatus = GameStatus
+
+type PrevGameState = GameState
+type NextGameState = GameState
+
+type ColorToPlay = Piece
+
+makeMove :: GameMove -> PrevGameState -> Hopefully NextGameState
 makeMove
-  (PhaseOneMove spot)
-  (GameState { status = OnGoing xToMove
-             , phase = PhaseOne
+  move
+  GameState { status = s@(OnGoing xToMove)
+             , phase = thePhase
              , board = brd
              , redPlData = redData
              , blackPlData = blackData
-             }) = undefined
+             } =
+  if not phaseMatchesMove move thePhase
+  then Left $ Error "Phase != Move Phase"
+  else do
+    nextBrd <- boardStep xToMove move brd
+    let nextPlData = plDataStep (nextBrd, (redData, blackData))
+    let nextPhase = phaseStep PhaseOne nextPlData
+    let nextStatus = statusStep s move nextBrd
+    return GameState { redPlData = fst nextPlData
+                    , blackPlData = snd nextPlData
+                    , phase = nextPhase
+                    , board = nextBrd
+                    , status = nextStatus
+                    }
+makeMove _ _ = Left $ Error "Can't move in an end state"
 
-makeMove _ _ = undefined
+phaseMatchesMove :: GameMove -> GamePhase -> Bool
+phaseMatchesMove (PhaseOneMove _)   PhaseOne = True
+phaseMatchesMove (PhaseTwoMove _ _) PhaseTwo = True
+phaseMatchesMove _                  _        = False
 
 
 
+boardStep ::
+  ColorToPlay -> GameMove -> PrevBoard -> Hopefully NextBoard
+boardStep pieceToPlay move brd = undefined
 
-boardPlace :: PieceIndex -> SixBoard -> Hopefully SixBoard
-boardPlace = undefined
 
--- Say (red, black)
-type SixPlayers = (PlayerData, PlayerData)
+plDataStep :: (NextBoard, PrevPlayerData) -> NextPlayerData
+plDataStep = undefined
 
-newPlayerData :: GameMove -> SixPlayers -> SixPlayers
-newPlayerData = undefined
 
--- args: curr phase, updated player data
-newPhase :: GamePhase -> SixPlayers -> GamePhase
-newPhase = undefined
+phaseStep :: PrevPhase -> NextPlayerData -> NextPhase
+phaseStep PhaseTwo _ = PhaseTwo
+phaseStep PhaseOne (p1, p2) =
+  if all ((== 0) . toPlayCt) [p1,p2]
+  then PhaseTwo
+  else PhaseOne
+
+statusStep :: PrevStatus -> GameMove -> NextBoard -> NextStatus
+statusStep = undefined
 
 
 
