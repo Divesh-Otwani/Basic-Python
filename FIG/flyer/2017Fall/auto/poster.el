04282e1dad1ee957ae05c38fbd4e7d995f1189b1
Divesh Otwani <divesh.otwani@gmail.com>
2017-09-28 13:20:43 -0400

    Commit at time Thu Sep 28 13:20:43 EDT 2017

---
+++ FIG/flyer/2017Fall/auto/poster.el
@@ -0,0 +1,50 @@
+(TeX-add-style-hook
+ "poster"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "english") ("geometry" "top=0.8in" "bottom=1in" "left=1.05in" "right=1.10in" "headheight=0in" "headsep=3in")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "lineno"
+    "fix-cm"
+    "amsfonts"
+    "babel"
+    "amssymb"
+    "latexsym"
+    "amsthm"
+    "amsmath"
+    "verbatim"
+    "geometry"
+    "graphicx")
+   (TeX-add-symbols
+    '("f" 1)
+    '("comt" 1)
+    "R"
+    "Q"
+    "N"
+    "Z"
+    "Ps"
+    "ba"
+    "spc")
+   (LaTeX-add-amsthm-newtheorems
+    "thm"
+    "lem"
+    "cor"
+    "prop"
+    "crit"
+    "alg"
+    "defn"
+    "conj"
+    "exmp"
+    "exmps"
+    "prob"
+    "rem"
+    "note"
+    "claim"
+    "summ"
+    "case"
+    "ack"))
+ :latex)
+
